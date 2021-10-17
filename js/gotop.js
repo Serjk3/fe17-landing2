91088b973976e5be3f6b343dc7a944e5398a516d
Serjk3 <serj.ryzhko@gmail.com>
2021-10-17 17:02:34 +0300

    add interactivity

--- js/gotop.js
+++ js/gotop.js
@@ -1,18 +1,27 @@
-$(document).ready(function(){  
-    //fade in/out based on scrollTop value
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 0) {
-            $('.gotop').fadeIn();
-        } else {
-            $('.gotop').fadeOut();
-        }
-    });
- 
-    // scroll body to 0px on click
-    $('.gotop').click(function () {
-        $('body,html').animate({
-            scrollTop: 0
-        }, 400);
-        return false;
-    });
+$(document).ready(function () {
+  //fade in/out based on scrollTop value
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 0) {
+      $(".gotop").fadeIn();
+      $(".header").css({ backgroundColor: "rgba(53, 60, 101, 0.9)" });
+      $(".header").css({ paddingBottom: "0" });
+      $(".header-wrapper").css({ borderBottom: "0" });
+    } else {
+      $(".gotop").fadeOut();
+      $(".header").css({ backgroundColor: "transparent" });
+      $(".header").css({ paddingBottom: "25px" });
+      $(".header-wrapper").css({ borderBottom: "1px solid #babbc5" });
+    }
+  });
+
+  // scroll body to 0px on click
+  $(".gotop").click(function () {
+    $("body,html").animate(
+      {
+        scrollTop: 0,
+      },
+      400
+    );
+    return false;
+  });
 });
